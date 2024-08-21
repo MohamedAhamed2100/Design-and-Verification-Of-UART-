337670d2769e9be4741caab7694b4db7e7e9da64
MohamedAhamed2100 <107947222+MohamedAhamed2100@users.noreply.github.com>
2024-08-21 17:08:40 +0300

    Add files via upload

---
+++ UART_TX/Serializer.v
@@ -0,0 +1,48 @@
+module Serializer #( parameter IN_DATA_WIDTH = 8)
+(
+     input wire [IN_DATA_WIDTH-1:0] P_DATA  , // Paramitrized Parallel Input Data  
+     input wire             Ser_Enable , // synchronous Serializer Enable signal
+	 input wire                    CLK , // Clock Signal
+	 input wire                    RST , // Asynchronous Reset Signal
+	 input wire                   BUSY , // synchronous Busy Flag
+	 input wire             Data_Valid , // synchronous Data_Valid Flag 
+	 output                   Ser_Done , // serializer Done Flag 
+	 output                    Ser_Data  // serializer Output data 
+
+);
+
+
+reg [$clog2(IN_DATA_WIDTH)-1:0] Ser_Count ; // serializer Counter 
+reg [IN_DATA_WIDTH-1:0]        Ser_P_DATA ; // serializer Parallel Data for processing
+
+/****************** Load Serializer Input **************/
+always @(posedge CLK , negedge RST)
+begin
+     if(~RST)
+	      Ser_P_DATA <= 'b0 ;
+	  else if(Data_Valid && !BUSY)
+         Ser_P_DATA <= P_DATA ;
+     else if(Ser_Enable)
+         Ser_P_DATA <= Ser_P_DATA >> 1 ;      
+end 
+
+
+/****************** Serializer Counter **************/
+always @(posedge CLK , negedge RST )
+begin
+     if(~RST)
+	     Ser_Count <= 'b0 ;
+	 else	
+	 begin
+         if(Ser_Enable)
+	        Ser_Count <= Ser_Count + 'b1 ;
+         else
+		    Ser_Count <= 'b0 ;
+	 end	    
+end 
+
+/******************* Assign Serializer Outputs **********************/
+assign Ser_Data = Ser_P_DATA[0] ;
+assign Ser_Done = (Ser_Count == IN_DATA_WIDTH-1) ? 1'b1 : 1'b0 ;
+
+endmodule 
---
+++ UART_TX/UART_TX_FSM.v
@@ -0,0 +1,128 @@
+module UART_TX_FSM (
+   input wire       CLK        , // clock signal
+   input wire       RST        , // Asynchronous active low reset signal 
+   input wire       PAR_EN     , // parity calculator enable 
+   input wire       ser_done   , // serializer done flag
+   input wire       Data_Valid , // synchronous data valid flag 
+   output reg [1:0] mux_sel    , // mux selector 
+   output reg       ser_en     , // serializer enable 
+   output reg       busy         // synchronous busy flag 
+);
+
+/***************** defined States of FSM  ****************/
+parameter [2:0]  IDLE   = 3'b000,
+                 start  = 3'b001,
+			     data   = 3'b011,
+			     parity = 3'b010,
+			     stop   = 3'b110 ; 
+				 
+reg [2:0] Current_State , Next_State ; // Synchroubized states 
+reg busy_comb; 
+
+/****************** State Transition *********************/
+always@(posedge CLK , negedge RST ) 
+begin 
+     if( ~RST )
+         Current_State <= IDLE ;
+	 else
+	     Current_State <= Next_State ;
+end 
+
+/*********************************************************/
+/********* Algorirhmic State Machine Chart (ASM) *********/  
+/*********************************************************/
+
+always @(*)
+begin 
+     case(Current_State)
+	 
+	     IDLE  : begin
+		         if(Data_Valid)
+				     Next_State = start ;
+			     else 
+				     Next_State =  IDLE ;
+				 end
+				 
+	     start : Next_State = data ;	
+		 
+		 data  : begin
+		         if(ser_done)
+				     begin 
+				         if(PAR_EN)
+				             Next_State = parity ;
+					     else 
+					         Next_State = stop ;
+                     end       					 
+			     else 
+				     Next_State =  data ;	 
+				 end
+				 
+		 parity: Next_State = stop ;
+		 
+		 stop  : Next_State = IDLE ;
+		 
+		 default: Next_State = IDLE ;
+		 
+	 endcase
+	 
+end
+
+
+always @(*)
+begin 
+
+     mux_sel   = 2'b00;
+	 busy_comb = 1'b0 ;
+	 ser_en    = 1'b0 ;
+				 
+     case(Current_State)
+	 
+	     IDLE  : begin
+		         mux_sel   = 2'b11;
+				 busy_comb = 1'b0 ;
+				 end
+				 
+	     start : begin
+				 busy_comb = 1'b1 ;
+				 end	
+		 
+		 data  : begin
+		         mux_sel   = 2'b10;
+				 busy_comb = 1'b1 ;
+				 ser_en    = 1'b1 ;
+				 if (ser_done)
+				    ser_en = 1'b0 ;
+				 else
+				    ser_en = 1'b1 ;
+				 end
+				 
+		 parity: begin
+		         mux_sel   = 2'b11;
+				 busy_comb = 1'b1 ;
+				 end
+		 
+		 stop  : begin
+		         mux_sel   = 2'b01;
+				 busy_comb = 1'b1 ;
+				 end
+		 
+		 default: begin
+                 mux_sel   = 2'b00;
+	             busy_comb = 1'b0 ;
+	             ser_en    = 1'b0 ;
+				 end
+		 
+	 endcase
+end
+
+/*************** busy flag ************/
+always@(posedge CLK , negedge RST ) 
+begin 
+     if( ~RST )
+         busy <= 1'b0 ;
+	 else
+	     busy <= busy_comb ;
+end 
+
+endmodule 
+
